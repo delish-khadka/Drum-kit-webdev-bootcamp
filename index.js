f9564dfee2fbb543195fad52427a5a9e0763c28b
delish-khadka <np05cp4a190051@iic.edu.np>
2022-10-19 22:40:46 +0545

    completed

---
+++ index.js
@@ -0,0 +1,56 @@
+var numberOfButtons = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < numberOfButtons; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+    var buttondata = this.innerHTML;
+    makeSound(buttondata);
+    buttonAnimation(buttondata);
+  });
+
+  document.addEventListener("keydown", (event) => {
+    makeSound(event.key);
+    buttonAnimation(event.key);
+  });
+}
+
+function makeSound(key) {
+  switch (key) {
+    case "w":
+      var sound = new Audio("/sounds/crash.mp3");
+      sound.play();
+      break;
+    case "a":
+      var sound = new Audio("/sounds/kick-bass.mp3");
+      sound.play();
+      break;
+    case "s":
+      var sound = new Audio("/sounds/snare.mp3");
+      sound.play();
+      break;
+    case "d":
+      var sound = new Audio("/sounds/tom-1.mp3");
+      sound.play();
+      break;
+    case "j":
+      var sound = new Audio("/sounds/tom-2.mp3");
+      sound.play();
+      break;
+    case "k":
+      var sound = new Audio("/sounds/tom-3.mp3");
+      sound.play();
+      break;
+    case "l":
+      var sound = new Audio("/sounds/tom-4.mp3");
+      sound.play();
+      break;
+  }
+}
+
+function buttonAnimation(currentKey) {
+  var activebutton = document.querySelector("." + currentKey);
+  activebutton.classList.add("pressed");
+
+  setTimeout(() => {
+    activebutton.classList.remove("pressed");
+  }, 100);
+}
